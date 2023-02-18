57920fbb031927abcc6bf237d0ba032328719a9f
Pınar Suvaçoğlu <pinarsuvac@gmail.com>
2023-02-18 20:52:05 +0300

    add solution p2

---
+++ 01 - Calorie Counting/solution-p2.js
@@ -0,0 +1,25 @@
+import { parseLines } from "../utils/index.js";
+
+const lines = parseLines("./01 - Calorie Counting/input.txt", true);
+
+const ObjectArray = [...Object.values(lines)];
+
+const allElves = [];
+let elfCarrier = [];
+
+ObjectArray.map((key, value) => {
+	if (key !== "") {
+		elfCarrier.push(key);
+	} else {
+		allElves.push(elfCarrier);
+    elfCarrier = [];
+	}
+});
+
+const totalCaloriesForElves = [];
+
+Object.values(allElves).forEach(elf => {
+	totalCaloriesForElves.push(elf.reduce((acc, item) => acc + parseInt(item), 0))
+});
+
+console.log(totalCaloriesForElves.sort().reverse().slice(0,3).reduce((acc, item) => acc + parseInt(item), 0));
